1a64ac0a162f75c9d09e24af3dd23bfb7e2a9eef
Ostapenko Nataliia <nataliia.ostapenko@student.itmont.de>
2024-03-28 09:27:24 +0100

    HW1 done

---
+++ HW-1/index.js
@@ -0,0 +1,11 @@
+// Привітатись з юзером
+alert("Hello!");
+
+// Спитати імя
+const name = prompt("What is your name?");
+
+// Спитати з якої він країни
+const country = prompt("Where are you from?");
+
+// Вивести на екран
+alert("Nice to meet you, " + name + "! You are from " + country + "!");
